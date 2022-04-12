76fcb85322cc4eaa3474bd6cb0525758621b4428
jongwon417 <jongwon417@gmail.com>
2022-04-12 13:57:23 +0900

    lcd

---
+++ lcd2/lcd2.ino
@@ -0,0 +1,41 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.print("JongWon");
+  lcd.setCursor(0,1);
+  lcd.print("CDS:");
+  lcd.setCursor(9,1);
+  lcd.print("LED:");
+
+  pinMode(A0,INPUT);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  int val = analogRead(A0);  
+
+  lcd.setCursor(4,1);
+  lcd.print("    ");
+  lcd.setCursor(4,1);
+  lcd.print(val);
+  
+  String led;
+  if(val>100){
+    digitalWrite(13,0);
+    led="OFF";
+  }
+  else{
+    digitalWrite(13,1);
+    led="ON";
+  }
+  lcd.setCursor(13,1);
+  lcd.print("   ");
+  lcd.setCursor(13,1);
+  lcd.print(led);
+  delay(500);
+}
